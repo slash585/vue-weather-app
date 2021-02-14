e9e1ff7217c3fd5f71d6ada5b6ffe10f80d98c20
slash585 <51036040+slash585@users.noreply.github.com>
2021-02-14 21:39:07 +0300

    Initializing app

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name:"search"
+}
+</script>
+
+<template lang="pug">
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend/src/components/Weather.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'weather'
+}
+</script>
+
+<template lang="pug">
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
