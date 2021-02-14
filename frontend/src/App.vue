2d98652bc19a1443f0c799c12e02735463dd0c8c
slash585 <51036040+slash585@users.noreply.github.com>
2021-02-14 22:30:53 +0300

    Create search bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,53 @@
 <script>
-export default {
+import Search from '@/components/Search.vue'
+import Weather from '@/components/Weather.vue'
 
+export default {
+  name:"app",
+  components:{
+    Search,
+    Weather
+  }
 }
 </script>
 
 <template lang="pug">
-  
+  #app
+    main
+      Search
+      Weather
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+body{
+    font-family: 'Avenir Next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4;
+}
+
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+
 </style>
--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,13 +1,52 @@
 <script>
 export default {
-    name:"search"
+    name:"search",
+    data(){
+        return{
+            query: ""
+        }
+    },
+    methods:{
+        fetchWeather(){
+            console.log(this.query)
+        }
+    }
 }
 </script>
 
 <template lang="pug">
-
+.search
+  .search-box
+    input.search-bar(type="text" placeholder="Search..." v-model="query" @keypress.enter="fetchWeather")
 </template>
 
 <style lang="scss" scoped>
+.search-box{
+    width: 100%;
+    margin-bottom: 30px;
+    .search-bar{
+        display: block;
+        width: 100%;
+        padding: 15px;
+
+        color: #313131;
+        font-size: 20px;
+
+        appearance: none;
+        border: none;
+        outline: none;
+        background: none;
+
+        box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+        background-color: rgba(255, 255, 255, 0.5);
+        border-radius: 0px 16px 0px 16px;
+        transition: 0.4s;
+        &:focus{
+            box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+            background-color: rgba(255, 255, 255, 0.75);
+            border-radius: 16px 0px 16px 0px;
+        }
+    }
+}
 
 </style>
